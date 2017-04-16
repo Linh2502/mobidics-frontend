5ec0a066879ad23d2c65c471aa3732e94c541789
Long Bui <longbui68@yahoo.de>
2017-04-17 01:57:59 +0200

    updated angular and typescript version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
@@ -6,18 +7,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { AuthService } from "./services/auth/auth.service";
-import { ExportService } from "./services/export/export.service";
-import { HttpService } from "./services/http/http.service";
-import { SearchService } from "./services/search/search.service";
+import { AuthService } from './services/auth/auth.service';
+import { ExportService } from './services/export/export.service';
+import { HttpService } from './services/http/http.service';
+import { SearchService } from './services/search/search.service';
 import { HeaderComponent } from './navbar/navbar.component';
-import { CollapseModule } from "ngx-bootstrap/collapse";
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { SidemenuComponent } from './home/main/sidemenu/sidemenu.component';
 import { MethodComponent } from './home/main/method/method.component';
 import { MethodListComponent } from './home/main/method/method-list/method-list.component';
 import { MethodDetailComponent } from './home/main/method/method-detail/method-detail.component';
 import { MethodAddComponent } from './home/main/method/method-add/method-add.component';
-import { routing } from "./app.routing";
+import { routing } from './app.routing';
 import { MainComponent } from './home/main/main.component';
 import { AccountComponent } from './home/account/account.component';
 import { MethodEditComponent } from './home/main/method/method-edit/method-edit.component';
@@ -50,6 +51,7 @@ import { AccountDetailsComponent } from './home/account/account-details/account-
     FormsModule,
     HttpModule,
     CollapseModule.forRoot(),
+    BrowserAnimationsModule,
     routing
   ],
   providers: [
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,10 @@
-import { RouterModule, Routes } from "@angular/router";
-import { AccountComponent } from "./home/account/account.component";
-import { MainComponent } from "./home/main/main.component";
-import { METHOD_ROUTES } from "./home/main/method/method.routing";
-import { LoginComponent } from "./login/login.component";
-import { ACCOUNT_ROUTES } from "./home/account/account.routing";
-import { AdminCenterComponent } from "./home/admin-center/admin-center.component";
+import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './home/account/account.component';
+import { MainComponent } from './home/main/main.component';
+import { METHOD_ROUTES } from './home/main/method/method.routing';
+import { LoginComponent } from './login/login.component';
+import { ACCOUNT_ROUTES } from './home/account/account.routing';
+import { AdminCenterComponent } from './home/admin-center/admin-center.component';
 
 const APP_ROUTES: Routes = [
   { path: 'login', component: LoginComponent },
