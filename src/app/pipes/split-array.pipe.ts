41dd8722d541b76673cd32744271de93316bb1a8
Bui, Giao Long <giaolong.bui@student.fhws.de>
2017-06-03 18:57:19 +0200

    added split-array.pipe.ts

---
+++ src/app/pipes/split-array.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'splitArray'
+})
+export class SplitArrayPipe implements PipeTransform {
+
+  transform(value: any, args?: any): any {
+    let result: string = null;
+    if (value) {
+      let splitString: string[] = value.split('::');
+      result = splitString[0];
+      for (let i = 1; i < splitString.length; i++) {
+        result += ', ' + splitString[i];
+      }
+    }
+    return result;
+  }
+
+}
